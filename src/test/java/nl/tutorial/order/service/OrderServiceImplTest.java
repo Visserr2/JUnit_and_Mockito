677c8f12066882fabe4753abb258a36b807dfcf3
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2017-09-19 19:36:09 +0200

    Used times, atleast and anyMatchers
    Used times, atleast and anyMatchers

--- src/test/java/nl/tutorial/order/service/OrderServiceImplTest.java
+++ src/test/java/nl/tutorial/order/service/OrderServiceImplTest.java
@@ -2,6 +2,8 @@ package nl.tutorial.order.service;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -10,6 +12,7 @@ import java.sql.SQLException;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import nl.tutorial.order.dao.OrderDAO;
@@ -19,6 +22,7 @@ import nl.tutorial.order.exception.BOException;
 public class OrderServiceImplTest {
 	
 	private static final int ORDER_ID = 123;
+	
 	@Mock
 	private OrderDAO dao;
 	private OrderServiceImpl service;
@@ -36,12 +40,12 @@ public class OrderServiceImplTest {
 	@Test
 	public void placeOrder_Should_Create_Order() throws SQLException, BOException{	
 		// Setting Expectation
-		when(dao.create(order)).thenReturn(new Integer(1));			
+		when(dao.create(Mockito.any(Order.class))).thenReturn(new Integer(1));			
 		boolean result = service.placeOrder(order);
 		
 		// verify the result
 		//1. Check if dao.create gets called once or more
-		verify(dao).create(order);
+		verify(dao, times(1)).create(order);
 		
 		//2. Check if the result is true
 		assertTrue(result);
@@ -50,12 +54,12 @@ public class OrderServiceImplTest {
 	@Test
 	public void placeOrder_Should_Not_Create_Order() throws SQLException, BOException{			
 		// Setting Expectation
-		when(dao.create(order)).thenReturn(new Integer(0));			
+		when(dao.create(Mockito.any(Order.class))).thenReturn(new Integer(0));			
 		boolean result = service.placeOrder(order);
 		
 		// verify the result
 		//1. Check if dao.create gets called once or more
-		verify(dao).create(order);
+		verify(dao, atLeast(1)).create(order);
 		
 		//2. Check if the result is false
 		assertFalse(result);
@@ -64,15 +68,15 @@ public class OrderServiceImplTest {
 	@Test(expected=BOException.class)
 	public void placeOrder_Should_Throw_BOException() throws SQLException, BOException{			
 		// Setting Expectation - throws SQL Exception
-		when(dao.create(order)).thenThrow(SQLException.class);			
+		when(dao.create(Mockito.any(Order.class))).thenThrow(SQLException.class);			
 		service.placeOrder(order);
 	}
 	
 	@Test
 	public void cancelOrder_Should_Cancel_The_Order() throws SQLException, BOException {
 		// setting expectations
-		when(dao.read(ORDER_ID)).thenReturn(order);
-		when(dao.update(order)).thenReturn(1);		
+		when(dao.read(Mockito.anyInt())).thenReturn(order);
+		when(dao.update(Mockito.any(Order.class))).thenReturn(1);		
 		
 		// execute methods
 		boolean result = service.candelOrder(ORDER_ID);
@@ -88,8 +92,8 @@ public class OrderServiceImplTest {
 	@Test
 	public void cancelOrder_Should_Not_Cancel_The_Order() throws SQLException, BOException {
 		// setting expectations
-		when(dao.read(ORDER_ID)).thenReturn(order);
-		when(dao.update(order)).thenReturn(0);		
+		when(dao.read(Mockito.anyInt())).thenReturn(order);
+		when(dao.update(Mockito.any(Order.class))).thenReturn(0);		
 		
 		// execute methods
 		boolean result = service.candelOrder(ORDER_ID);
@@ -105,7 +109,7 @@ public class OrderServiceImplTest {
 	@Test(expected=BOException.class)
 	public void cancelOrder_ReadMethod_Should_Throw_BOException() throws SQLException, BOException {
 		// setting expectations
-		when(dao.read(ORDER_ID)).thenThrow(SQLException.class);
+		when(dao.read(Mockito.anyInt())).thenThrow(SQLException.class);
 		
 		// execute methods
 		service.candelOrder(ORDER_ID);	
@@ -114,8 +118,8 @@ public class OrderServiceImplTest {
 	@Test(expected=BOException.class)
 	public void cancelOrder_UpdateMethod_Should_Throw_BOException() throws SQLException, BOException {
 		// setting expectations
-		when(dao.read(ORDER_ID)).thenReturn(order);
-		when(dao.update(order)).thenThrow(SQLException.class);
+		when(dao.read(Mockito.anyInt())).thenReturn(order);
+		when(dao.update(Mockito.any(Order.class))).thenThrow(SQLException.class);
 		
 		// execute methods
 		service.candelOrder(ORDER_ID);	
@@ -123,7 +127,7 @@ public class OrderServiceImplTest {
 	
 	@Test
 	public void deleteOrder_Deletes_The_Order() throws SQLException, BOException {
-		when(dao.delete(ORDER_ID)).thenReturn(1);
+		when(dao.delete(Mockito.anyInt())).thenReturn(1);
 		boolean result = service.deleteOrder(ORDER_ID);
 		
 		verify(dao).delete(ORDER_ID);
@@ -134,7 +138,7 @@ public class OrderServiceImplTest {
 	
 	@Test
 	public void deleteOrder_Does_Not_Delete_The_Order() throws SQLException, BOException {
-		when(dao.delete(ORDER_ID)).thenReturn(0);
+		when(dao.delete(Mockito.anyInt())).thenReturn(0);
 		boolean result = service.deleteOrder(ORDER_ID);
 		
 		verify(dao).delete(ORDER_ID);
@@ -145,7 +149,7 @@ public class OrderServiceImplTest {
 	
 	@Test(expected=BOException.class)
 	public void deleteOrder_Throws_BOException() throws SQLException, BOException {
-		when(dao.delete(ORDER_ID)).thenThrow(SQLException.class);
+		when(dao.delete(Mockito.anyInt())).thenThrow(SQLException.class);
 		service.deleteOrder(ORDER_ID);
 	}
 
